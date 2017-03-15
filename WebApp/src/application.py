c2a9a58e9f4bfd9efa0dfd4b5534079f20961731
ksk-uchi <ksk.uchi+github@gmail.com>
2017-03-15 19:32:59 +0900

    temp

--- WebApp/src/application.py
+++ WebApp/src/application.py
@@ -2,13 +2,15 @@
 import os
 import logging
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 from app_config import BaseConfig, DevConfig
 from app_logging import setup_logging
 from views import top
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 # 'APP_ENVIRONMENT' is defined by Dockerfile
 app_env = os.getenv('APP_ENVIRONMENT', 'devel')
 
@@ -18,13 +20,24 @@ config = {
 }
 app.config.from_object(config[app_env])
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
+db.init_app(app)
+
 # logging Singleton object definition
 setup_logging(env=app_env)
 logger = logging.getLogger(__name__)
 
 # Loading views
 app.register_blueprint(top.app, url_prefix='/top')
-
+logger.debug(app.config)
 
 @app.errorhandler(500)
 def handle_500_response(e):
--- WebApp/src/views/top.py
+++ WebApp/src/views/top.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, request, current_app, abort
+from flask import (
+    Blueprint,
+    request,
+    current_app,
+    render_template
+)
 from logging import getLogger
 
 app = Blueprint('top', __name__)
 logger = getLogger('views.api')
 
-@app.route('/')
 def hello_top():
-    return 'こんちわ'
+    logger.debug('here')
+    variables = {
+        'proper_text': 'こんちわ',
+        'whois': '三河屋',
+        'b_and_s': ['サザエ', 'かつお', 'わかめ']
+    }
+    return render_template('toppage.html', **variables)
+
+# This view including below URLs.
+app.add_url_rule('/', 'top_index', hello_top)
